b9815b02c4258c67bdfd08a5068e16b59823cfca
Egor Rasolko <egor.rassolko2018@gmail.com>
2022-04-23 00:28:16 +0300

    fixed solution string,-regexp,-datatime

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 //String, RegExp, DataTime
+
+//  Дана строка 'ahb acb aeb aeeb adcb axeb'.
+//  Напишите регулярное выражение, которое найдет строки ahb, acb, aeb по шаблону: буква 'a', любой символ, буква 'b'
 function show1() {
   let str = "ahb acb aeb aeeb adcb axeb".replace(/a.b/g, "+");
   console.log(str);
 }
 show1();
-
+// Дана строка '2+3 223 2223'. Напишите регулярку, которая найдет строку 2+3, не захватив остальные
 function show2() {
   let str = "2+3 223 2223".replace(/2\+3/g, "!");
   console.log(str);
 }
 show2();
-
+// Получить день, месяц и год текущей даты и по отдельности вывести в консоль
 let date = new Date();
 let year = date.getFullYear();
-let month = date.getMonth();
-let day = date.getDay();
+let month = date.getMonth() + 1;
+let day = date.getDate();
 console.log(year);
 console.log(month);
 console.log(day);
