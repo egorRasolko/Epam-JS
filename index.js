e3cb405bdaf8bdd453bd75df11f3755c2d7204ed
Egor Rasolko <egor.rassolko2018@gmail.com>
2022-05-17 13:58:35 +0300

    fixed solution for Exception-handling

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 //Exception handling
 
 // В try catch конструкцию завернуть код: console.log(a), let a = 3.
-//  И вывести ошибку – ‘let перед использованием нужно объявить’. 
+//  И вывести ошибку – ‘let перед использованием нужно объявить’.
 // При выполнении 1/0  выводить ошибку 'на ноль делить нельзя'
- 
-const checkError = (a, b) => {
-  try {
-    if (!a) {
-      throw "Переменную надо задать";
+
+try {
+  console.log(a);
+  let a = 3;
+} catch (e) {
+  console.log("let перед использованием нужно объявить");
+}
+
+function divFunc(a, b) {
+  const res = a / b;
+  if (res === Infinity || isNaN(res)) {
+    try{
+      throw new Error("На ноль делить нельзя");
+    }catch(e){
+      console.log(e.message);
     }
-    if (b === 0) {
-      throw "На ноль делить нельзя";
-    }
-    console.log(a / b);
-  } catch (e) {
-    console.log(e);
   }
-};
-xxx( );
+  return res;
+}
+divFunc(1, 0);
+
